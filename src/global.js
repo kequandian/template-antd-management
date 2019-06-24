416f593d14ac281a6e34f696b7fbac1ec57a6797
LLH <youyeke@163.com>
2019-06-24 16:34:58 +0800

    setEndpoint 别名

--- src/global.js
+++ src/global.js
@@ -1,9 +1,9 @@
 import ZEle_antd from 'zero-element-antd';
 
-// import { set } from 'zero-element/lib/utils/request/endpoint';
+import { set as setEndpoint } from 'zero-element/lib/utils/request/endpoint';
 // import { saveToken } from 'zero-element/lib/utils/request/token';
 
-// set('http://192.168.1.1:8000');
+// setEndpoint('http://192.168.1.1:8000');
 
 // saveToken({
 //   token: 'token value',
